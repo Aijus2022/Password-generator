c89b479403140976d3710dd371c2a0f0f1dfa88f
Aijamal Jusupova <109210016+Aijus2022@users.noreply.github.com>
2023-12-19 23:00:30 +0000

    Update script.js
    changed the code to add more functions.

--- script.js
+++ script.js
@@ -1,13 +1,46 @@
-    function generatePassword() {
-      const passwordLength = document.getElementById("passwordLength").value;
-      const lowercase = document.getElementById("lowercase").checked;
-      const uppercase = document.getElementById("uppercase").checked;
-      const numeric = document.getElementById("numeric").checked;
-      const special = document.getElementById("special").checked;
+     function generatePassword() {
+      // Prompt for password length
+      let passwordLength = prompt("Enter password length (8-128 characters):");
+      passwordLength = parseInt(passwordLength);
 
-      // Validation and password generation logic here
+      // Validate password length
+      if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+        alert("Invalid password length. Please enter a number between 8 and 128.");
+        return;
+      }
+
+      // Prompt for character types
+      let lowercase = confirm("Include lowercase characters?");
+      let uppercase = confirm("Include uppercase characters?");
+      let numeric = confirm("Include numeric characters?");
+      let special = confirm("Include special characters?");
+
+      // Validate at least one character type is selected
+      if (!lowercase && !uppercase && !numeric && !special) {
+        alert("At least one character type must be selected.");
+        return;
+      }
+
+      // Define character sets based on user choices
+      const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+      const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      const numericChars = "0123456789";
+      const specialChars = "$@%&*";
+
+      // Build character set for password
+      let characters = "";
+      if (lowercase) characters += lowercaseChars;
+      if (uppercase) characters += uppercaseChars;
+      if (numeric) characters += numericChars;
+      if (special) characters += specialChars;
+
+      // Generate password
+      let password = "";
+      for (let i = 0; i < passwordLength; i++) {
+        const randomIndex = Math.floor(Math.random() * characters.length);
+        password += characters.charAt(randomIndex);
+      }
 
       // Display password
-      alert("Generated Password:\n" + generatedPassword);
+      alert("Generated Password:\n" + password);
     }
- 
