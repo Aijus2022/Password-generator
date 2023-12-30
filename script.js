1aebe5601cfd7d34f105d254a50f727827e68059
Aijamal Jusupova <109210016+Aijus2022@users.noreply.github.com>
2023-12-30 03:17:47 +0000

    Update script.js
    made all app's elements to be displayed directly on the screen instead of in an alert box.

--- script.js
+++ script.js
@@ -1,46 +1,70 @@
-       function generatePassword() {
-      // Prompt for password length
-      let passwordLength = prompt("Enter password length (8-128 characters):");
-      passwordLength = parseInt(passwordLength);
+function generatePassword() {
+  // Get the elements
+  var passwordLengthInput = document.getElementById("password-length");
+  var lowercaseCheckbox = document.getElementById("lowercase");
+  var uppercaseCheckbox = document.getElementById("uppercase");
+  var numericCheckbox = document.getElementById("numeric");
+  var specialCheckbox = document.getElementById("special");
+  var passwordDisplay = document.getElementById("password-display");
 
-      // Validate password length
-      if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
-        alert("Invalid password length. Please enter a number between 8 and 128.");
-        return;
-      }
+  // Get user input values
+  var passwordLength = parseInt(passwordLengthInput.value);
+  var includeLowercase = lowercaseCheckbox.checked;
+  var includeUppercase = uppercaseCheckbox.checked;
+  var includeNumeric = numericCheckbox.checked;
+  var includeSpecial = specialCheckbox.checked;
 
-      // Prompt for character types
-      let lowercase = confirm("Include lowercase characters?");
-      let uppercase = confirm("Include uppercase characters?");
-      let numeric = confirm("Include numeric characters?");
-      let special = confirm("Include special characters?");
+  // Validate user input
+  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+    passwordDisplay.innerHTML = "<p>Please enter a valid password length (between 8 and 128).</p>";
+    return;
+  }
 
-      // Validate at least one character type is selected
-      if (!lowercase && !uppercase && !numeric && !special) {
-        alert("At least one character type must be selected.");
-        return;
-      }
+  if (!(includeLowercase || includeUppercase || includeNumeric || includeSpecial)) {
+    passwordDisplay.innerHTML = "<p>Please select at least one character type.</p>";
+    return;
+  }
 
-      // Define character sets based on user choices
-      const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
-      const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-      const numericChars = "0123456789";
-      const specialChars = "$@%&*";
+  // Generate the password
+  var password = generateRandomPassword(passwordLength, includeLowercase, includeUppercase, includeNumeric, includeSpecial);
 
-      // Build character set for password
-      let characters = "";
-      if (lowercase) characters += lowercaseChars;
-      if (uppercase) characters += uppercaseChars;
-      if (numeric) characters += numericChars;
-      if (special) characters += specialChars;
+  // Display the password on the page
+  passwordDisplay.innerHTML = "<p>Generated Password:</p><p>" + password + "</p>";
+}
 
-      // Generate password
-      let password = "";
-      for (let i = 0; i < passwordLength; i++) {
-        const randomIndex = Math.floor(Math.random() * characters.length);
-        password += characters.charAt(randomIndex);
-      }
+function generateRandomPassword(length, includeLowercase, includeUppercase, includeNumeric, includeSpecial) {
+  // Function to generate a random password based on criteria
+  // Implementation logic goes here...
 
-      // Display password
-      alert("Generated Password:\n" + password);
-    }
+  // For demonstration purposes, a simple implementation is used.
+  var charset = "";
+  var lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  var uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var numericChars = "0123456789";
+  var specialChars = "$@%&*";
+
+  if (includeLowercase) {
+    charset += lowercaseChars;
+  }
+
+  if (includeUppercase) {
+    charset += uppercaseChars;
+  }
+
+  if (includeNumeric) {
+    charset += numericChars;
+  }
+
+  if (includeSpecial) {
+    charset += specialChars;
+  }
+
+  var password = "";
+  for (var i = 0; i < length; i++) {
+    var randomIndex = Math.floor(Math.random() * charset.length);
+    password += charset.charAt(randomIndex);
+  }
+
+  return password;
+}
+
